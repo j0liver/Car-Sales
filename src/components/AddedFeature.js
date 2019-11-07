9060800affd5fb53a0cf9bb912dc238a048dc4a5
Joe O <j0liverko@gmail.com>
2019-11-06 19:32:08 -0600

    Changed Added feature to use hook rather then connect hoc

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect, useDispatch } from 'react-redux'
 import { removeFeature } from '../components/actions/index'
 const AddedFeature = props => {
+  const dispatch = useDispatch()
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={()=>props.removeFeature(props.feature)}>X</button>
+      <button className="button" onClick={()=>dispatch(removeFeature(props.feature))}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default connect(
-  null,
-  {removeFeature}
-)(AddedFeature);
+export default AddedFeature
+// export default connect(
+//   null,
+//   {removeFeature}
+// )(AddedFeature);
+
+// onClick={()=>props.removeFeature(props.feature)}
